dbc0cbdd093f4cc1db02c3b9b75cdea322b7cce1
oudream <oudream@126.com>
2020-04-13 07:54:38 +0800

    add projects lishi-db-tool

---
+++ projects/lishi-db-tool/mainwindow.cpp
@@ -0,0 +1,42 @@
+#include "mainwindow.h"
+#include "./ui_mainwindow.h"
+
+#include <QWebEngineView>
+#include <QWebEnginePage>
+#include <QWebEngineProfile>
+#include <QStringList>
+#include <QUrl>
+
+#include <ccxx/ccxx.h>
+
+QUrl commandLineUrlArgument()
+{
+    const QStringList args = QCoreApplication::arguments();
+    for (const QString &arg : args.mid(1)) {
+        if (!arg.startsWith(QLatin1Char('-')))
+            return QUrl::fromUserInput(arg);
+    }
+    return QUrl(QStringLiteral("https://www.baidu.com"));
+}
+
+QUrl url = commandLineUrlArgument();
+
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+    QWebEngineView *webView = new QWebEngineView(ui->pnLeft);
+    QWebEnginePage *webPage = new QWebEnginePage(QWebEngineProfile::defaultProfile(), webView);
+    webView->setPage(webPage);
+    webView->setUrl(url);
+    ui->pnLeft->layout()->addWidget(webView);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
